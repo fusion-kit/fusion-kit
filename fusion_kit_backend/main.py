abc72299fa6edd9294bbfd4b85a9688d74e94aad
Kyle Lacy <kylelacy@kyle.space>
2022-09-08 00:26:33 -0700

    Replace HTTP API endpoint with GraphQL

--- fusion_kit_backend/main.py
+++ fusion_kit_backend/main.py
@@ -1,15 +1,39 @@
 import sys
 sys.path.append('./stable_diffusion')
 
-from flask import Flask
+from ariadne import gql, ObjectType, make_executable_schema
+from ariadne.asgi import GraphQL
+from starlette.applications import Starlette
+import uvicorn
 import tasks
 
-app = Flask("fusion-kit")
+type_defs = gql("""
+    type Query {
+        hello: String!
+    }
 
-@app.route("/")
-def hello_world():
+    type Mutation {
+        text_to_image(prompt: String!): String!
+    }
+""")
+
+query = ObjectType("Query")
+
+@query.field("hello")
+def resolve_hello(*_):
+    return "Hello world!"
+
+mutation = ObjectType("Mutation")
+
+@mutation.field("text_to_image")
+def resolve_text_to_image(*_, prompt):
     tasks.txt2img()
     return "Generation complete"
 
+schema = make_executable_schema(type_defs, query, mutation)
+
+app = Starlette(debug=True)
+app.mount("/graphql", GraphQL(schema, debug=True))
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    uvicorn.run(app, host='0.0.0.0', port=2425)
