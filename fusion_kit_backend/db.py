6c370ef49bd0d858ea44d4896bbc272900f02fae
Kyle Lacy <kylelacy@kyle.space>
2022-09-15 01:06:43 -0700

    Set up SQLAlchemy models for `dreams` and `dream_images` tables

--- fusion_kit_backend/db.py
+++ fusion_kit_backend/db.py
@@ -1,6 +1,10 @@
 import alembic.config
 import alembic.command
 import sqlalchemy
+import sqlalchemy.orm
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+
+Session = sqlalchemy.orm.sessionmaker()
 
 class DbConfig():
     def __init__(self, alembic_ini_path, alembic_script_path, db_path):
@@ -14,6 +18,7 @@ class DbConfig():
         self.db_url = f"sqlite:///{db_path}?mode=rwc"
 
         self.db_engine = sqlalchemy.create_engine(self.db_url)
+        Session.configure(bind=self.db_engine)
 
 def run_db_migrations(db_config, db_conn):
     alembic_cfg = alembic.config.Config(db_config.alembic_ini_path)
@@ -22,3 +27,27 @@ def run_db_migrations(db_config, db_conn):
     alembic_cfg.set_main_option("sqlalchemy.url", db_config.db_url)
 
     alembic.command.upgrade(alembic_cfg, "head")
+
+Base = sqlalchemy.orm.declarative_base()
+
+class Dream(Base):
+    __tablename__ = 'dreams'
+
+    id = Column(String, primary_key=True)
+    prompt = Column(String, nullable=False)
+    seed = Column(Integer, nullable=False)
+    num_images = Column(Integer, nullable=False)
+    settings_json = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default=func.current_timestamp())
+
+class DreamImage(Base):
+    __tablename__ = 'dream_images'
+
+    id = Column(String, primary_key=True)
+    dream_id = Column(String, ForeignKey('dreams.id'), nullable=False)
+    seed = Column(Integer, nullable=False)
+    index = Column(Integer, nullable=False)
+    image_path = Column(String, nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    blurhash = Column(String, nullable=False)
