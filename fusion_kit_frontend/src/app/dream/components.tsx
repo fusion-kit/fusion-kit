043fa19cb0bd57be6a3c94531bbc04b393bb0de4
Kyle Lacy <kylelacy@kyle.space>
2022-09-19 23:36:17 -0700

    Add action buttons when viewing an image

--- fusion_kit_frontend/src/app/dream/components.tsx
+++ fusion_kit_frontend/src/app/dream/components.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { clsx } from "clsx";
+import { ArrowDownTrayIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import { BeakerIcon, BookmarkIcon } from "@heroicons/react/24/solid";
 import {
   DreamImage, getDreamImageUri, isDreamImageLoading, getDreamImageProgress,
 } from "./hooks";
@@ -15,37 +17,68 @@ export const ShowDreamImage: React.FC<ShowDreamImageProps> = (props) => {
   const isLoading = isDreamImageLoading(image);
   const progress = getDreamImageProgress(image);
 
-  if (imageUri != null) {
-    return (
-      <BigImageContainer widthRatio={1} heightRatio={1}>
-        <div
-          className={clsx(
-            "transition-[border-radius] overflow-hidden relative",
-            progress != null ? "rounded-lg" : "",
-          )}
-        >
-          <img src={imageUri} alt="" className="w-full h-full object-contain" />
-          {progress != null ? (
-            <div className="h-[10%] absolute inset-x-0 bottom-0">
-              <MiniProgressBar progress={progress} />
+  return (
+    <div className="flex flex-col lg:h-full">
+      <div className="min-h-0">
+        <BigImageContainer widthRatio={1} heightRatio={1}>
+          {imageUri != null ? (
+            <div
+              className={clsx(
+                "transition-[border-radius] overflow-hidden relative",
+                progress != null ? "rounded-lg" : "",
+              )}
+            >
+              <img src={imageUri} alt="" className="w-full h-full object-contain" />
+              {progress != null ? (
+                <div className="h-[10%] absolute inset-x-0 bottom-0">
+                  <MiniProgressBar progress={progress} />
+                </div>
+              ) : null}
             </div>
-          ) : null}
-        </div>
-      </BigImageContainer>
-    );
-  } else {
-    return (
-      <BigImageContainer widthRatio={1} heightRatio={1}>
-        <div
-          className={clsx(
-            "bg-gray-200 w-full h-full rounded-lg",
-            isLoading ? "animate-pulse" : "",
+          ) : (
+            <div
+              className={clsx(
+                "bg-gray-200 w-full h-full rounded-lg",
+                isLoading ? "animate-pulse" : "",
+              )}
+            >
+            </div>
           )}
-        >
-        </div>
-      </BigImageContainer>
-    );
-  }
+        </BigImageContainer>
+      </div>
+      <div className="mx-auto py-2 max-w-lg lg:max-w-full lg:pb-0 flex space-x-2">
+        <DreamImageActionButton label="Bookmark">
+          <BookmarkIcon className="h-8 w-8" aria-hidden="true" />
+        </DreamImageActionButton>
+        <DreamImageActionButton label="Download">
+          <ArrowDownTrayIcon className="h-8 w-8" aria-hidden="true" />
+        </DreamImageActionButton>
+        <DreamImageActionButton label="Open in new window">
+          <ArrowTopRightOnSquareIcon className="h-full w-full" aria-hidden="true" />
+        </DreamImageActionButton>
+        <DreamImageActionButton label="Edit in lab">
+          <BeakerIcon className="h-full w-full" aria-hidden="true" />
+        </DreamImageActionButton>
+      </div>
+    </div>
+  );
+};
+
+type DreamImageActionButtonProps = React.PropsWithChildren<{
+  label: string,
+}>;
+
+const DreamImageActionButton: React.FC<DreamImageActionButtonProps> = (props) => {
+  return (
+    <button
+      type="button"
+      title={props.label}
+      className="flex h-10 w-10 p-1.5 items-center justify-center rounded-full text-gray-400 border-2 border-gray-400 hover:text-gray-600 hover:border-gray-600 focus:outline-none focus:border-blue-600 focus:text-blue-600"
+    >
+      {props.children}
+      <span className="sr-only">{props.label}</span>
+    </button>
+  );
 };
 
 type BigImageContainerProps = React.PropsWithChildren<{
