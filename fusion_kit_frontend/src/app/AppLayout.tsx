40bd5f2a042fe8916b11bbe35cb3942da088b1c1
Kyle Lacy <kylelacy@kyle.space>
2022-09-27 16:23:06 -0700

    Remove unused profile section from sidebar

--- fusion_kit_frontend/src/app/AppLayout.tsx
+++ fusion_kit_frontend/src/app/AppLayout.tsx
@@ -104,25 +104,6 @@ export const AppLayout: React.FC<React.PropsWithChildren> = (props) => {
                     </div>
                   </nav>
                 </div>
-                <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-                  <a href="#" className="group block flex-shrink-0">
-                    <div className="flex items-center">
-                      <div>
-                        <img
-                          className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                          Whitney Francis
-                        </p>
-                        <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
               </Dialog.Panel>
             </Transition.Child>
             <div className="w-14 flex-shrink-0" aria-hidden="true">
@@ -170,23 +151,6 @@ export const AppLayout: React.FC<React.PropsWithChildren> = (props) => {
                   ))}
                 </div>
               </nav>
-            </div>
-            <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-              <a href="#" className="group block w-full flex-shrink-0">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Whitney Francis</p>
-                    <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                  </div>
-                </div>
-              </a>
             </div>
           </div>
         </div>
